b93bce7687e77fcd28412079357d07419c4c64aa
VictoriaH <vicky627243@gmail.com>
2023-10-06 16:46:14 -0300

    Change: se modifica la ruta del formulario para subir la imagen

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ const productType = require('./models/productType.js');
 const dd = require('dump-die');
 const path = require('path');
 const { error } = require('console');
+const { upload } = require('./models/product.js');
 
 const router = express.Router();
 
@@ -34,8 +35,7 @@ router.get('/formulario', async(req,res) => {
     }
 });
 
-
-router.post('/formulario', async (req, res) => {
+router.post('/formulario', upload.single('urlImagen'), async (req, res) => {
     const productData = req.body;
     try{
         const newProduct = await ProductModel.createProduct(productData);
